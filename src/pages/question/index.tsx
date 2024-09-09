21c9b89ddb51280012e0c1537fcfd223863536f3
JarvisChan666 <744765386@qq.com>
2024-09-09 13:14:14 +0800

    finish question css

--- src/pages/question/index.tsx
+++ src/pages/question/index.tsx
@@ -4,6 +4,7 @@ import { AtButton, AtRadio } from 'taro-ui'
 import './index.scss'
 import Taro from '@tarojs/taro'
 import questions from '../../data/questions.json'
+import { useState } from 'react'
 
 export default () => {
   // print the json file
@@ -11,35 +12,39 @@ export default () => {
   const question = questions[0]
 
   const questionOptions = question.options.map((option) => {
-    return { label: `${option.key}. ${option.value}`, value: option.key}
+    return { label: `${option.key}. ${option.value}`, value: option.key }
   })
+  // 定义题号，状态（影响页面更新）
+  const [questionIndex, setQuestionIndex] = useState<number>(1)
+
   return (
     <View className='question'>
-      <View className='at-article__h1'>{question.title}</View>
+      <View className='at-article__h1'>{questionIndex}. {question.title}</View>
       <AtRadio
         options={questionOptions}
       // value={this.state.value}
       // onClick={this.handleChange.bind(this)}
       />
 
-      <AtButton className='header-button' type='primary' circle={true} onClick={() => {
+      <AtButton className='controlBtn' type='primary' circle={true}  onClick={() => {
         // Implement the navigation logic here
         Taro.navigateTo({ url: '/pages/question/index' });
       }}>
         下一题
       </AtButton>
-      <AtButton className='header-button' type='primary' circle={true} onClick={() => {
-        // Implement the navigation logic here
-        Taro.navigateTo({ url: '/pages/question/index' });
-      }}>
-        上一题
-      </AtButton>
-      <AtButton className='header-button' type='primary' circle={true} onClick={() => {
+      <AtButton className='controlBtn' type='primary' circle={true} onClick={() => {
         // Implement the navigation logic here
         Taro.navigateTo({ url: '/pages/question/index' });
       }}>
         查看结果
       </AtButton>
+      <AtButton className='controlBtn' circle={true} onClick={() => {
+        // Implement the navigation logic here
+        Taro.navigateTo({ url: '/pages/question/index' });
+      }}>
+        上一题
+      </AtButton>
+      
     </View>
   )
 }
